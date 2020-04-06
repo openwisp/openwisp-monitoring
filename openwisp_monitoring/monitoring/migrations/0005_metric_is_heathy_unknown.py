0bec2e604b7798fc7284ad4323e2cbc1f1c71bc9
Federico Capoano <federico.capoano@gmail.com>
2020-04-06 11:09:47 -0500

    [monitoring] Added blank=True to 0005_metric_is_heathy_unknown

--- openwisp_monitoring/monitoring/migrations/0005_metric_is_heathy_unknown.py
+++ openwisp_monitoring/monitoring/migrations/0005_metric_is_heathy_unknown.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='metric',
             name='is_healthy',
-            field=models.BooleanField(db_index=True, default=None, null=True),
+            field=models.BooleanField(db_index=True, default=None, null=True, blank=True),
         ),
     ]
