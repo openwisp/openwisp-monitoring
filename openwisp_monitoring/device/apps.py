b33e643884a10757ed1f8df2052734e710f0e23e
Federico Capoano <federico.capoano@gmail.com>
2020-04-15 19:49:30 -0500

    [models] Use generic relation to get device metrics and checks #9
    Related to #9

--- openwisp_monitoring/device/apps.py
+++ openwisp_monitoring/device/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_delete
 from django.utils.translation import ugettext_lazy as _
 
 from .utils import manage_short_retention_policy
@@ -11,3 +12,21 @@ class DeviceMonitoringConfig(AppConfig):
 
     def ready(self):
         manage_short_retention_policy()
+        self.connect_device_post_delete()
+
+    def connect_device_post_delete(self):
+        from openwisp_controller.config.models import Device
+
+        post_delete.connect(
+            self.device_post_delete_receiver,
+            sender=Device,
+            dispatch_uid='device_post_delete_receiver',
+        )
+
+    @classmethod
+    def device_post_delete_receiver(cls, instance, **kwargs):
+        from ..device.models import DeviceData
+
+        instance.__class__ = DeviceData
+        instance.checks.all().delete()
+        instance.metrics.all().delete()
--- openwisp_monitoring/monitoring/apps.py
+++ openwisp_monitoring/monitoring/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 from django.conf import settings
-from django.db.models.signals import post_delete
 from django.utils.translation import ugettext_lazy as _
 
 from .utils import create_database
@@ -15,20 +14,3 @@ class MonitoringConfig(AppConfig):
         # create influxdb database if doesn't exist yet
         create_database()
         setattr(settings, 'OPENWISP_ADMIN_SHOW_USERLINKS_BLOCK', True)
-        from ..device.models import Device
-
-        post_delete.connect(
-            self.delete_related_metric,
-            sender=Device,
-            dispatch_uid='delete_related_metric',
-        )
-
-    @classmethod
-    def delete_related_metric(cls, instance, **kwargs):
-        from .models import Metric
-
-        Metric.objects.filter(
-            object_id=instance.pk,
-            content_type__app_label=instance._meta.app_label,
-            content_type__model=instance._meta.model_name,
-        ).delete()
