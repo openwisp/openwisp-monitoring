64a1b0e956b19376256d550bba265f16a2789ec1
Pablo Castellano <pablo@anche.no>
2020-04-16 19:31:05 +0200

    [refactor] Move get_device_data logic to DeviceData model #31
    Fixes #31

--- openwisp_monitoring/device/admin.py
+++ openwisp_monitoring/device/admin.py
@@ -1,12 +1,8 @@
-from datetime import datetime
-
-from dateutil.relativedelta import relativedelta
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
-from pytz import timezone as tz
 
 from openwisp_controller.config.admin import DeviceAdmin as BaseDeviceAdmin
 from openwisp_controller.config.models import Device
@@ -17,41 +13,6 @@ from .models import DeviceData, DeviceMonitoring
 
 
 class DeviceAdmin(BaseDeviceAdmin):
-    def get_device_data(self, data):
-        if not data:
-            return None
-        if 'general' in data and 'local_time' in data['general']:
-            local_time = data['general']['local_time']
-            data['general']['local_time'] = datetime.fromtimestamp(
-                local_time, tz=tz('UTC')
-            )
-        if 'general' in data and 'uptime' in data['general']:
-            uptime = '{0.days} days, {0.hours} hours and {0.minutes} minutes'
-            data['general']['uptime'] = uptime.format(
-                relativedelta(seconds=data['general']['uptime'])
-            )
-        if 'resources' in data and 'memory' in data['resources']:
-            # convert bytes to megabytes
-            MB = 1000000.0
-            for key in data['resources']['memory'].keys():
-                data['resources']['memory'][key] /= MB
-        remove = []
-        for interface in data.get('interfaces', []):
-            # don't show interfaces if they don't have any useful info
-            if len(interface.keys()) <= 2:
-                remove.append(interface)
-                continue
-            # human readable mode
-            interface['wireless']['mode'] = interface['wireless']['mode'].replace(
-                '_', ' '
-            )
-            # convert to GHz
-            if 'wireless' in interface and 'frequency' in interface['wireless']:
-                interface['wireless']['frequency'] /= 1000
-        for interface in remove:
-            data['interfaces'].remove(interface)
-        return data
-
     def get_extra_context(self, pk=None):
         ctx = super(DeviceAdmin, self).get_extra_context(pk)
         if pk:
@@ -59,7 +20,7 @@ class DeviceAdmin(BaseDeviceAdmin):
             api_url = reverse('monitoring:api_device_metric', args=[pk])
             ctx.update(
                 {
-                    'device_data': self.get_device_data(device_data.data),
+                    'device_data': device_data.data_user_friendly,
                     'api_url': api_url,
                     'default_time': Graph.DEFAULT_TIME,
                 }
