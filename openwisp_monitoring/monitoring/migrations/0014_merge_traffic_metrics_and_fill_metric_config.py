9e842c668f12bf55fc0039f078b704f3a453b113
Hardik Jain <54471024+nepython@users.noreply.github.com>
2020-07-07 07:33:23 +0530

    [metric-config] Add fill metric configuration migration

---
+++ openwisp_monitoring/monitoring/migrations/0014_merge_traffic_metrics_and_fill_metric_config.py
@@ -0,0 +1,64 @@
+from django.db import migrations
+from swapper import load_model
+
+
+metric_mapping = {
+    'reachable': 'ping',
+    'rx_bytes': 'traffic',
+    'clients': 'clients',
+    'used_disk': 'disk',
+    'percent_used': 'memory',
+    'cpu_usage': 'cpu',
+}
+
+
+def merge_traffic_metrics(apps, schema_editor):
+    Metric = load_model('monitoring', 'Metric')
+    Chart = load_model('monitoring', 'Chart')
+    rx_metrics = Metric.objects.filter(field_name='rx_bytes')
+    for rx_metric in rx_metrics:
+        if rx_metric.name.split()[1] == 'traffic':
+            return
+        # Traffic chart is created with tx_bytes metric
+        tx_metric = Metric.objects.get(name=f'{rx_metric.name.split()[0]} tx_bytes')
+        if tx_metric.chart_set.count():
+            chart = Chart.objects.get(metric=tx_metric)
+            new_name = f'{rx_metric.name.split()[0]} traffic'
+            print(f'Renamed metric "{rx_metric.name}" to "{new_name}"')
+            rx_metric.name = new_name
+            rx_metric.save()
+            chart.metric = rx_metric
+            chart.save()
+        tx_metric.delete()
+
+
+def find_metric_configuration(field_name):
+    try:
+        value = metric_mapping[field_name]
+        return value
+    except KeyError:
+        print(f'No metric configuration found for "{field_name}"')
+
+
+def fill_configuration(apps, schema_editor):
+    Metric = apps.get_model('monitoring', 'Metric')
+    for metric in Metric.objects.all():
+        metric.configuration = find_metric_configuration(metric.field_name)
+        metric.full_clean()
+        metric.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('monitoring', '0013_metric_configuration'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            merge_traffic_metrics, reverse_code=migrations.RunPython.noop
+        ),
+        migrations.RunPython(
+            fill_configuration, reverse_code=migrations.RunPython.noop
+        ),
+    ]
