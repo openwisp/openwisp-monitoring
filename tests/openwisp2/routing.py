459aa42d40d058231d4867609656b94647fcf4df
Federico Capoano <federico.capoano@gmail.com>
2020-07-29 11:41:12 -0500

    [tests] Added geo_routes from controller

--- tests/openwisp2/routing.py
+++ tests/openwisp2/routing.py
@@ -1,7 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from openwisp_notifications.websockets.routing import get_routes
 
+from openwisp_controller.geo.channels.routing import geo_routes
+
 application = ProtocolTypeRouter(
-    {'websocket': AuthMiddlewareStack(URLRouter(get_routes()))}
+    {
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(get_routes() + geo_routes))
+        )
+    }
 )
