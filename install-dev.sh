8ff5b28f2d5db587c50f0680c0994fbce173b2d0
Federico Capoano <federico.capoano@gmail.com>
2020-08-07 18:33:37 -0500

    [tests] Use openwisp-notifications master in tests

--- install-dev.sh
+++ install-dev.sh
@@ -5,4 +5,4 @@ pip install -U https://github.com/openwisp/openwisp-controller/tarball/master
 # TODO: removed when openwisp-users 0.3.0 is released
 pip install -U https://github.com/openwisp/openwisp-users/tarball/master
 # TODO: remove when openwisp-notifications 0.1 is released
-pip install -U https://github.com/openwisp/openwisp-notifications/tarball/dev
+pip install -U https://github.com/openwisp/openwisp-notifications/tarball/master
