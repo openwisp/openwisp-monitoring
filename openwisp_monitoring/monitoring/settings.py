8319f08fce057b7721f0a1d2818203549ec8dcd4
Hardik Jain <54471024+nepython@users.noreply.github.com>
2020-08-15 02:09:52 +0530

    [feature] Made metric collection resilient to tsdb failures #166
    Write operations are moved to the background where
    they are retried automatically indefinitely until the write
    succeeds.
    
    This makes the system fault tolerant toward temporary
    tsdb outages due to excessive load or data center issues.
    
    Implements and closes #166

--- openwisp_monitoring/monitoring/settings.py
+++ openwisp_monitoring/monitoring/settings.py
@@ -2,3 +2,11 @@ from django.conf import settings
 
 ADDITIONAL_CHARTS = getattr(settings, 'OPENWISP_MONITORING_CHARTS', {})
 ADDITIONAL_METRICS = getattr(settings, 'OPENWISP_MONITORING_METRICS', {})
+
+RETRY_OPTIONS = getattr(
+    settings,
+    'OPENWISP_MONITORING_WRITE_RETRY_OPTIONS',
+    dict(
+        max_retries=None, retry_backoff=True, retry_backoff_max=600, retry_jitter=True,
+    ),
+)
