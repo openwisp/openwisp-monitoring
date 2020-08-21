85ae661dbf3fd4ac62b7b794d1749fdbda666676
Federico Capoano <federico.capoano@gmail.com>
2020-08-20 19:49:49 -0500

    [ci] Install notifications dev branch (squashme)

--- install-dev.sh
+++ install-dev.sh
@@ -3,5 +3,4 @@
 # TODO: removed when openwisp-controller 0.8.0 is released
 pip install -U https://github.com/openwisp/openwisp-controller/tarball/master
 # TODO: remove when openwisp-notifications 0.1 is released
-# Change to "dev" branch before merging
-pip install -U https://github.com/openwisp/openwisp-notifications/tarball/issues/5-disable-notifications
+pip install -U https://github.com/openwisp/openwisp-notifications/tarball/dev
