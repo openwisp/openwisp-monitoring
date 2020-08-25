17fbced11c287ca1d1daf00d77777b21f260209d
Hardik Jain <hardikashishjain@gmail.com>
2020-08-25 20:40:22 +0530

    [fix] Fixed create_ping and create_config migrations #205

--- openwisp_monitoring/check/migrations/0003_create_ping.py
+++ openwisp_monitoring/check/migrations/0003_create_ping.py
@@ -5,12 +5,16 @@ from openwisp_monitoring.check.tasks import auto_create_ping
 
 def create_device_ping(apps, schema_editor):
     if AUTO_PING:
+        ContentType = apps.get_model('contenttypes', 'ContentType')
+        Check = apps.get_model('check', 'Check')
         Device = apps.get_model('config', 'Device')
         for device in Device.objects.all():
             auto_create_ping(
                 model=Device.__name__.lower(),
                 app_label=Device._meta.app_label,
                 object_id=str(device.pk),
+                check_model=Check,
+                content_type_model=ContentType,
             )
 
 
--- openwisp_monitoring/check/migrations/0005_create_config_applied.py
+++ openwisp_monitoring/check/migrations/0005_create_config_applied.py
@@ -6,12 +6,16 @@ from openwisp_monitoring.check.tasks import auto_create_config_check
 def add_config_applied_checks(apps, schema_editor):
     if not AUTO_CONFIG_CHECK:
         return
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    Check = apps.get_model('check', 'Check')
     Device = apps.get_model('config', 'Device')
     for device in Device.objects.all():
         auto_create_config_check(
             model=Device.__name__.lower(),
             app_label=Device._meta.app_label,
             object_id=str(device.pk),
+            check_model=Check,
+            content_type_model=ContentType,
         )
 
 
