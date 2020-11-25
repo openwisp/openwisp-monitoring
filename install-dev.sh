1776c90bb23c93e3425c530a6d3a282a87bfaa7f
Federico Capoano <federico.capoano@gmail.com>
2020-11-25 12:55:05 -0500

    [deps] Updated openwisp-controller to 0.8

--- install-dev.sh
+++ install-dev.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# TODO: removed when openwisp-controller 0.8.0 is released
-pip install -U https://github.com/openwisp/openwisp-controller/tarball/master
-# TODO: remove when openwisp-notifications 0.1 is released
-# TODO: Change branch from dev to master
-pip install -U https://github.com/openwisp/openwisp-notifications/tarball/dev
+exit 0
