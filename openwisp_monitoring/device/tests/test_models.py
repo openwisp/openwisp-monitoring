178e1d99608b228ca2e9e6a75612e1e16a73c82c
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-04-12 22:23:01 +0530

    [test] Added test for auto clear management ip

--- openwisp_monitoring/device/tests/test_models.py
+++ openwisp_monitoring/device/tests/test_models.py
@@ -600,6 +600,17 @@ class TestDeviceMonitoring(CreateConnectionsMixin, BaseTestCase):
         self.assertEqual(dm.status, 'critical')
         self.assertIsNone(dm.device.management_ip)
 
+    @patch('openwisp_monitoring.device.settings.AUTO_CLEAR_MANAGEMENT_IP', False)
+    def test_management_ip_not_clear_device_online(self):
+        dm, ping, load, process_count = self._create_env()
+        dm.device.management_ip = '0.0.0.0'
+        dm.device.save()
+        self.assertEqual(dm.status, 'ok')
+        self.assertEqual(dm.device.management_ip, '0.0.0.0')
+        ping.check_threshold(0)
+        self.assertEqual(dm.status, 'critical')
+        self.assertIsNotNone(dm.device.management_ip)
+
     def _set_env_unknown(self, load, process_count, ping, dm):
         dm.status = 'unknown'
         dm.save()
