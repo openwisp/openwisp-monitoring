a48c54804aad793f0665d0542bbdd1f241c83835
Gagan Deep <pandafy.dev@gmail.com>
2022-01-06 22:29:10 +0530

    [chores] Fixed test project websocket routing

--- tests/openwisp2/routing.py
+++ tests/openwisp2/routing.py
@@ -1,14 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from openwisp_notifications.websockets.routing import get_routes
 
-from openwisp_controller.geo.channels.routing import geo_routes
+from openwisp_controller.routing import get_routes
 
 application = ProtocolTypeRouter(
     {
         'websocket': AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(get_routes() + geo_routes))
+            AuthMiddlewareStack(URLRouter(get_routes()))
         )
     }
 )
