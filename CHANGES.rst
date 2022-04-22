a5cfdf50e7edbaad58027d0dacc47344dd092fc1
Gagan Deep <pandafy.dev@gmail.com>
2022-04-23 01:01:35 +0530

    [requested-changes] Fixed migration

--- CHANGES.rst
+++ CHANGES.rst
@@ -15,6 +15,9 @@ Backward incompatible changes
 - *Monitoring Template* is deprecated in favour of `openwisp monitoring packages <https://github.com/openwisp/openwrt-openwisp-monitoring#openwrt-openwisp-monitoring>`_.
   Follow the migration guide in `Migrating from monitoring scripts to monitoring packages <#migrating-from-monitoring-scripts-to-monitoring-packages>`_
   section of openwisp-monitoring documentation.
+- If you have made changes to the default *Monitoring Template*, then
+  create a backup of your template before running migrations. Running
+  migrations will make changes to the default *Monitoring Template*.
 
 Version 0.1.0 [2021-01-31]
 --------------------------
--- openwisp_monitoring/device/migrations/0003_update_template.py
+++ openwisp_monitoring/device/migrations/0003_update_template.py
@@ -1,5 +1,7 @@
 # Manually Created
 
+from collections import OrderedDict
+
 from django.db import migrations
 
 from . import (
@@ -17,13 +19,27 @@ def migrate_data(apps, schema_editor):
     Template = apps.get_model('config', 'Template')
     try:
         template = Template.objects.get(pk=TEMPLATE_MONITORING_UUID)
-        template.config['files'][0] = TEMPLATE_OPENWISP_MONITORING_02
-        template.config['files'][1] = TEMPLATE_NETJSON_MONITORING_02
-        template.config['files'][2] = TEMPLATE_CRONTAB_MONITORING_02
-        template.config['files'][3] = TEMPLATE_RC_LOCAL_02
-        template.config['files'][4] = TEMPLATE_UPDATE_OPENWISP_PACKAGES_02
-        template.config['files'][5] = TEMPLATE_POST_RELOAD_HOOK_02
-        template.config['openwisp'][0]['included_interfaces'] = '*'
+        template.config = OrderedDict(
+            {
+                'files': [
+                    TEMPLATE_OPENWISP_MONITORING_02,
+                    TEMPLATE_NETJSON_MONITORING_02,
+                    TEMPLATE_CRONTAB_MONITORING_02,
+                    TEMPLATE_RC_LOCAL_02,
+                    TEMPLATE_UPDATE_OPENWISP_PACKAGES_02,
+                    TEMPLATE_POST_RELOAD_HOOK_02,
+                ],
+                'openwisp': [
+                    OrderedDict(
+                        {
+                            'config_name': 'monitoring',
+                            'config_value': 'monitoring',
+                            'included_interfaces': '*',
+                        }
+                    )
+                ],
+            }
+        )
         template.full_clean()
         template.save()
     except Template.DoesNotExist:
