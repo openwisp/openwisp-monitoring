f89563034ee0244f5849b1e9ae1f922079968ad2
Aryamanz29 <aryamanz29@gmail.com>
2022-07-01 17:40:27 +0530

    [requested-changes] Set AUTO_IPERF to false

--- openwisp_monitoring/check/settings.py
+++ openwisp_monitoring/check/settings.py
@@ -12,6 +12,5 @@ AUTO_PING = get_settings_value('AUTO_PING', True)
 AUTO_CONFIG_CHECK = get_settings_value('AUTO_DEVICE_CONFIG_CHECK', True)
 MANAGEMENT_IP_ONLY = get_settings_value('MANAGEMENT_IP_ONLY', True)
 PING_CHECK_CONFIG = get_settings_value('PING_CHECK_CONFIG', {})
-# By default it should be disabled.
-AUTO_IPERF = get_settings_value('AUTO_IPERF', True)
+AUTO_IPERF = get_settings_value('AUTO_IPERF', False)
 IPERF_SERVERS = get_settings_value('IPERF_SERVERS', {})
