c58441afaad54584d5453906177455639d236365
Yashika Jotwani <yashikajotwani12@gmail.com>
2022-07-19 23:41:49 +0530

    [minor-changes] Updated get_group_map #26
    Fixes #26

--- openwisp_monitoring/views.py
+++ openwisp_monitoring/views.py
@@ -43,30 +43,28 @@ class MonitoringApiViewMixin:
         return start_date, end_date
 
     def get_group_map(self, time, request):
-        GROUP_MAP = Chart.GROUP_MAP
-        group_map = GROUP_MAP.copy()
+        group_map = Chart.GROUP_MAP
         daySpan = request.GET.get('dayspan')
-        if daySpan >= '0' and daySpan < '3':
-            time = '1d'
-            group_map[time] = '10m'
-        elif daySpan >= '3' and daySpan < '7':
-            value = daySpan / 3
-            value = value * 20 + 'm'
-            time = '3d'
-            group_map[time] = value
-        elif daySpan >= '7' and daySpan < '28':
-            value = daySpan / 7
-            value = value * 1 + 'h'
-            time = '7d'
-            group_map[time] = value
-        elif daySpan >= '28' and daySpan < '365':
-            value = daySpan / 28
-            value = value * 24 + 'h'
-            time = '30d'
-            group_map[time] = value
-        elif daySpan == '365':
-            time = '365d'
-            group_map[time] = '24h'
+        if daySpan:
+            daySpan = int(daySpan)
+            if daySpan >= 0 and daySpan < 3:
+                time = '1d'
+                group_map[time] = '10m'
+            elif daySpan >= 3 and daySpan < 7:
+                value = str((daySpan / 3) * 20) + 'm'
+                time = '3d'
+                group_map[time] = value
+            elif daySpan >= 7 and daySpan < 28:
+                value = str((daySpan / 7) * 1) + 'd'
+                time = '7d'
+                group_map[time] = value
+            elif daySpan >= 28 and daySpan < 365:
+                value = str((daySpan / 28) * 24) + 'h'
+                time = '30d'
+                group_map[time] = value
+            elif daySpan == 365:
+                time = '365d'
+                group_map[time] = '24h'
 
     def get(self, request, *args, **kwargs):
         key = request.GET.get('key')
