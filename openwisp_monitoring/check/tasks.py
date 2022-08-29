4edc1921904e068a717dff426a7078afd0b7954c
Aryamanz29 <aryamanz29@gmail.com>
2022-08-29 18:40:13 +0530

    [feature] Added support for multiple iperf servers #391
    Closes #391

--- openwisp_monitoring/check/tasks.py
+++ openwisp_monitoring/check/tasks.py
@@ -1,15 +1,22 @@
 import json
 import logging
+from time import monotonic
 
 from celery import shared_task
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+from redis import StrictRedis
 from swapper import load_model
 
 from .settings import CHECKS_LIST
 
 logger = logging.getLogger(__name__)
+# We need to be careful while utilising this time variable.
+# lock expires in 25 secs, just greater than the TCP + UDP test time
+CACHE_LOCK_EXPIRE = 25
+# Todo: Update redis host
+redis_client = StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
 
 
 def get_check_model():
@@ -60,7 +67,30 @@ def perform_check(uuid):
     except ObjectDoesNotExist:
         logger.warning(f'The check with uuid {uuid} has been deleted')
         return
-    result = check.perform_check()
+    if check.check_type not in ['openwisp_monitoring.check.classes.Iperf']:
+        result = check.perform_check()
+    else:
+        check_lock_id = 'ow_monitoring_iperf_check_lock'
+        # Timeout with a small diff, so we'll leave the lock delete
+        # to the cache if it's close to being auto-removed/expired
+        timeout_at = monotonic() + CACHE_LOCK_EXPIRE - 1.5
+
+        # Try to acquire a lock, or put task back on queue
+        lock_acquired = redis_client.set(check_lock_id, 'locked', nx=True)
+        if not lock_acquired:
+            logger.warning(
+                f'The Iperf server is already occupied by a device, so putting {check} back in queue'
+            )
+            # We need to be careful while utilising this time variable
+            # just greater than the TCP + UDP test time
+            perform_check.apply_async(args=[uuid], countdown=25)
+            return
+        try:
+            result = check.perform_check()
+        finally:
+            # Release the lock
+            if monotonic() < timeout_at:
+                redis_client.delete(check_lock_id)
     if settings.DEBUG:  # pragma: nocover
         print(json.dumps(result, indent=4, sort_keys=True))
 
