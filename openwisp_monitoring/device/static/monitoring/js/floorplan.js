154dcb696a3dbf64f51a3085de5c5cd683cc94b9
dee077 <deepanshu.sahu27@gmail.com>
2025-06-24 03:09:28 +0530

    [feature] Add multiple floorplan navigation

--- openwisp_monitoring/device/static/monitoring/js/floorplan.js
+++ openwisp_monitoring/device/static/monitoring/js/floorplan.js
@@ -3,17 +3,27 @@
 (function ($) {
   function openFloorPlan(url) {
     const $floorPlanContainer = $(`
-    <div id="floorplan-container">
-      <span id="close-floorplan">×</span>
-      <div id="floorplan-content"></div>
-    </div>
-  `);
+      <div id="floorplan-container">
+        <span id="floorplan-close-btn">×</span>
+        <div id="floorplan-content"></div>
+      </div>
+    `);
 
-    $("#device-map-container").append($floorPlanContainer);
+    const $floorNavigation = $(`
+      <div id="floorplan-navigation">
+        <div class="nav-arrow up-arrow"></div>
+        <div class="floorplan-navigation-body"></div>
+        <div class="nav-arrow down-arrow"></div>
+      </div>
+    `);
 
-    $("#close-floorplan").on("click", function () {
+    $("#device-map-container").append($floorPlanContainer, $floorNavigation);
+
+    $("#floorplan-close-btn").on("click", function () {
       $("#floorplan-container").remove();
+      $("#floorplan-navigation").remove();
     });
+
     $.ajax({
       url: url,
       method: "GET",
@@ -24,16 +34,49 @@
           $("#floorplan-content").html("<p>No floorplan data available.</p>");
           return;
         }
-        // Currenlty support only one floorplan
-        const firstNode = data.nodes[0];
-        const image = firstNode.image;
-        const netjsonData = {
-          type: "NetworkGraph",
-          nodes: [firstNode],
-          links: [],
-        };
-        console.log(netjsonData);
-        renderIndoorMap(netjsonData, image);
+
+        const floors = [...new Set(data.nodes.map((n) => n.floor))].sort(
+          (a, b) => a - b,
+        );
+
+        const $navBody = $floorNavigation
+          .find(".floorplan-navigation-body")
+          .empty();
+
+        floors.forEach((floorNum) => {
+          $navBody.append(`
+            <button class="floor-btn" data-floor="${floorNum}">
+              ${floorNum}
+            </button>
+          `);
+        });
+
+        $floorNavigation
+          .off("click", ".floor-btn")
+          .on("click", ".floor-btn", (e) => {
+            const floor = +$(e.currentTarget).data("floor");
+            showFloor(floor);
+          });
+
+        showFloor(floors[0]);
+
+        function showFloor(floorNum) {
+          const nodesThisFloor = data.nodes.filter((n) => n.floor === floorNum);
+          if (nodesThisFloor.length === 0) return;
+
+          const image = nodesThisFloor[0].image;
+          const netjsonData = {
+            type: "NetworkGraph",
+            nodes: nodesThisFloor,
+            links: [],
+          };
+
+          $("#floorplan-content").replaceWith(
+            `<div id="floorplan-content"></div>`,
+          );
+
+          renderIndoorMap(netjsonData, image);
+        }
       },
       error: function () {
         alert("Error loading floorplan coordinates.");
@@ -41,14 +84,13 @@
     });
   }
 
-  function renderIndoorMap(data, image) {
+  function renderIndoorMap(data, imageUrl) {
     const graph = new NetJSONGraph(data, {
       el: "#floorplan-content",
       render: "map",
-
       mapOptions: {
-        center: [30, 100],
-        zoom: 1.5,
+        center: [50, 50],
+        zoom: 1,
         zoomSnap: 0.3,
         minZoom: -1,
         maxZoom: 2,
@@ -72,27 +114,36 @@
         });
         return data;
       },
-      // Have to find a soultion for rendering coordinates correctly here
+
       onReady() {
         const map = this.leaflet;
-        map.eachLayer((layer) => {
-          if (layer._url) map.removeLayer(layer);
-        });
-        const img = new Image();
-        img.src = image;
-        img.onload = () => {
-          const w = img.width,
-            h = img.height,
-            bottomRight = map.unproject([0, h * 2], map.getMaxZoom() - 1),
-            upperLeft = map.unproject([w * 2, 0], map.getMaxZoom() - 1),
-            bounds = new L.LatLngBounds(bottomRight, upperLeft);
+        const image = new Image();
+        image.src = imageUrl;
 
-          L.imageOverlay(image, bounds).addTo(map);
-          map.setMaxBounds(bounds);
+        image.onload = function () {
+          const aspectRatio = image.width / image.height;
+          const h = image.height;
+          const w = image.width;
+          const zoom = map.getMaxZoom() - 1;
+          const bottomRight = map.unproject([0, h * 2], zoom);
+          const upperLeft = map.unproject([w * 2, 0], zoom);
+          const bounds = new L.LatLngBounds(bottomRight, upperLeft);
+
+          L.imageOverlay(imageUrl, bounds).addTo(map);
           map.fitBounds(bounds);
+          map.setMaxBounds(bounds);
+          map.setView([0, 0], 0.25);
         };
+
+        // Remove base tile layers
+        map.eachLayer((layer) => {
+          if (layer._url) {
+            map.removeLayer(layer);
+          }
+        });
       },
     });
+
     graph.render();
   }
 
