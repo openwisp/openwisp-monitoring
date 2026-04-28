dbf23593665b4cc7929823fc3ee76ec4585971ea
Alexandre Vincent <alexandre.vincent@stellar.tc>
2026-04-28 12:14:43 +0200

    [change] Add 5G signal data on device admin status tab

--- openwisp_monitoring/device/tests/test_api.py
+++ openwisp_monitoring/device/tests/test_api.py
@@ -805,6 +805,54 @@ class TestDeviceApi(AuthenticationMixin, TestGeoMixin, DeviceMonitoringTestCase)
                 {"lte": {"rsrp": -75.00, "rsrq": -8.00, "rssi": -51.00, "snr": 13.00}},
             )
 
+    def test_5g_mobile_properties(self):
+        org = self._create_org()
+        device = self._create_device(organization=org)
+        data = {
+            "type": "DeviceMonitoring",
+            "interfaces": [
+                {
+                    "name": "mobile0",
+                    "mac": "00:00:00:00:00:00",
+                    "mtu": 1900,
+                    "multicast": True,
+                    "txqueuelen": 1000,
+                    "type": "modem-manager",
+                    "up": True,
+                    "mobile": {
+                        "connection_status": "connected",
+                        "imei": "300000001234567",
+                        "manufacturer": "Sierra Wireless, Incorporated",
+                        "model": "MC7430",
+                        "operator_code": "50502",
+                        "operator_name": "YES OPTUS",
+                        "power_status": "on",
+                        "signal": {"5g": {"rsrp": -75, "rsrq": -8, "snr": 13}},
+                    },
+                }
+            ],
+        }
+        response = self._post_data(device.id, device.key, data)
+        self.assertEqual(response.status_code, 200)
+        mobile_data = DeviceData(pk=device.pk).data["interfaces"][0]["mobile"]
+        with self.subTest("check mobile interface static properties"):
+            self.assertEqual(mobile_data["connection_status"], "connected")
+            self.assertEqual(mobile_data["imei"], "300000001234567")
+            self.assertEqual(mobile_data["model"], "MC7430")
+            self.assertEqual(mobile_data["operator_code"], "50502")
+            self.assertEqual(mobile_data["operator_name"], "YES OPTUS")
+            self.assertEqual(mobile_data["power_status"], "on")
+        with self.subTest("ensure signal data is converted to float"):
+            # ensure numbers are stored as floats,
+            # lua cannot be forced to send floats so we need to force it
+            self.assertIsInstance(mobile_data["signal"]["5g"]["rsrp"], float)
+            self.assertIsInstance(mobile_data["signal"]["5g"]["rsrq"], float)
+            self.assertIsInstance(mobile_data["signal"]["5g"]["snr"], float)
+            self.assertDictEqual(
+                mobile_data["signal"],
+                {"5g": {"rsrp": -75.00, "rsrq": -8.00, "snr": 13.00}},
+            )
+
     def test_empty_mobile_signal_data(self):
         org = self._create_org()
         device = self._create_device(organization=org)
