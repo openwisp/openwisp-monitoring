8705607f1e157f3b109d14427412d2a8247dd50b
Pushpit Kamboj <pushpitkamboj@gmail.com>
2026-06-01 22:54:17 +0530

    [tests] Fix influxDB2 check integration setup

--- openwisp_monitoring/db/backends/influxdb2/tests.py
+++ openwisp_monitoring/db/backends/influxdb2/tests.py
@@ -861,6 +861,7 @@ class TestInfluxDB2CheckIntegration(
     def test_wifi_clients_check_round_trip(self):
         device = self._create_device()
         device_data = DeviceData(pk=device.pk)
+        device_data.data = {}
         sample_data = self._data()
         sample_data.pop("resources")
         device_data.writer.write(sample_data, current=False)
@@ -889,6 +890,7 @@ class TestInfluxDB2CheckIntegration(
     def test_data_collected_check_round_trip(self):
         device = self._create_device()
         device_data = DeviceData(pk=device.pk)
+        device_data.data = {}
         sample_data = self._data()
         sample_data.pop("resources")
         device_data.writer.write(sample_data, current=False)
