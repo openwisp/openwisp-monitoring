c7e66abd6ed7e43a74bb6bb010c531af47854659
Shravani Dalvi <shravanidalvi70@gmail.com>
2026-09-18 23:05:15 +0530

    Resized the dashboard map popup

---
+++ run_test_local.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Add the project directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Run pytest directly on the file
+import pytest
+pytest.main(["openwisp_monitoring/tests/test_selenium.py::TestDashboardMap::test_dashboard_map_popup_resizes_for_custom_status_labels"])
